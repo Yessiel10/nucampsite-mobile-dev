20f7c06229349dbb3a8043cdf7c0aece6015b77c
Yessiel <yessielito@hotmail.com>
2023-12-10 22:01:31 -0800

    Gestures Added

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,13 +1,59 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, PanResponder, Alert } from "react-native";
 import { Card, Icon } from "react-native-elements";
 import { baseUrl } from "../../shared/baseUrl";
 import * as Animatable from "react-native-animatable";
+import { useRef } from "react";
 
 const RenderCampsite = (props) => {
+  const view = useRef();
+
   const { campsite } = props;
+
+  const isLeftSwipe = ({ dx }) => dx < -200;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      view.current
+        .rubberBand(1000)
+        .then((endState) =>
+          console.log(endState.finished ? "finished" : "canceled")
+        );
+    },
+    onPanResponderEnd: (e, gestureState) => {
+      if (isLeftSwipe(gestureState)) {
+        Alert.alert(
+          "Add Favorite",
+          "Are you sure you wish to add " + campsite.name + " to favorites?",
+          [
+            {
+              text: "Cancel",
+              style: "cancel",
+              onPress: () => console.log("Cancel Pressed"),
+            },
+            {
+              text: "OK",
+              onPress: () =>
+                props.isFavorite
+                  ? console.log("Already set as a favorite")
+                  : props.markFavorite(),
+            },
+          ],
+          { cancelable: false }
+        );
+      }
+    },
+  });
+
   if (campsite) {
     return (
-      <Animatable.View animation="fadeInDownBig" duration={2000} delay={1000}>
+      <Animatable.View
+        animation="fadeInDownBig"
+        duration={2000}
+        delay={1000}
+        ref={view}
+        {...panResponder.panHandlers}
+      >
         <Card containerStyle={styles.cardContainer}>
           <Card.Image source={{ uri: baseUrl + campsite.image }}>
             <View style={{ justifyContent: "center", flex: 1 }}>
